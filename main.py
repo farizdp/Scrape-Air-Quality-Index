a811567a61a2d3ddbed772438941f38e81738aef
farizdp <farizdwipratama@gmail.com>
2023-05-23 09:12:55 +0700

    delete source 2, edit output & sendmessage

--- main.py
+++ main.py
@@ -17,30 +17,33 @@ if r1.status_code == 200:
     aqi_status_1 = html1.find('span', class_="aqi-status__text").string
     pollutant_1 = html1.find('p', class_="number-above-time").string
 
-r2 = requests.get('https://www.aqi.in/id/dashboard/indonesia/jakarta')
-if r2.status_code == 200:
-    html2 = BeautifulSoup(r2.content, "html.parser")
-    update_2 = html2.find('p', class_="card-location-time").string.replace('Pembaharuan Terakhir: ', '')
-    aqi_value_2 = html2.find('td', class_="AQI_toggle curr").string
-    aqi_status_2 = html2.find('td', class_="AQI_text-1").string
-    pollutant_2 = html2.find('span', class_="Pollutants_sensor_text pm25Val").string
+# r2 = requests.get('https://www.aqi.in/id/dashboard/indonesia/jakarta')
+# if r2.status_code == 200:
+#     html2 = BeautifulSoup(r2.content, "html.parser")
+#     update_2 = html2.find('p', class_="card-location-time").string.replace('Pembaharuan Terakhir: ', '')
+#     aqi_value_2 = html2.find('td', class_="AQI_toggle curr").string
+#     aqi_status_2 = html2.find('td', class_="AQI_text-1").string
+#     pollutant_2 = html2.find('span', class_="Pollutants_sensor_text pm25Val").string
 
-table = pt.PrettyTable()
-table.title = 'Air Quality Index Jakarta'
-table.field_names = ['Parameter', 'IQAir', 'AQI']
-table.align['Parameter'] = 'l'
-table.add_row(['Update',    str(update_1),      str(update_2)])
-table.add_row(['Value',     str(aqi_value_1),   str(aqi_value_2)])
-table.add_row(['Status',    str(aqi_status_1),  str(aqi_status_2)])
-table.add_row(['Pollutant', str(pollutant_1),   str(pollutant_2)])
+# table = pt.PrettyTable()
+# table.title = 'Air Quality Index Jakarta'
+# table.field_names = ['Parameter', 'IQAir', 'AQI']
+# table.align['Parameter'] = 'l'
+# table.add_row(['Update',    str(update_1),      str(update_2)])
+# table.add_row(['Value',     str(aqi_value_1),   str(aqi_value_2)])
+# table.add_row(['Status',    str(aqi_status_1),  str(aqi_status_2)])
+# table.add_row(['Pollutant', str(pollutant_1),   str(pollutant_2)])
 
-text = "AQI Monitoring Jakarta\n\n```{0}```".format(table)
+# text = "AQI Monitoring Jakarta\n\n```{0}```".format(table)
 
-im = Image.new("RGB", (640, 180), "white")
-draw = ImageDraw.Draw(im)
-path = os.path.dirname(__file__) + '/'
-font = ImageFont.truetype(path + "font/FreeMono.ttf", 15)
-draw.text((10, 10), str(table), font=font, fill="black")
+# im = Image.new("RGB", (640, 180), "white")
+# draw = ImageDraw.Draw(im)
+# path = os.path.dirname(__file__) + '/'
+# font = ImageFont.truetype(path + "font/FreeMono.ttf", 15)
+# draw.text((10, 10), str(table), font=font, fill="black")
 
-im.save(path + "out/aqi.png")
-bot.sendPhoto(id_telegram, photo=open(path + "out/aqi.png", 'rb'))
+# im.save(path + "out/aqi.png")
+# bot.sendPhoto(id_telegram, photo=open(path + "out/aqi.png", 'rb'))
+
+output = "AQI Monitoring Jakarta\n\nUpdate : {0}\nValue : {1}\nStatus : {2}\nPollutant : {3}".format(update_1, aqi_value_1, aqi_status_1, pollutant_1)
+bot.sendMessage(id_telegram, output, 'Markdown')
